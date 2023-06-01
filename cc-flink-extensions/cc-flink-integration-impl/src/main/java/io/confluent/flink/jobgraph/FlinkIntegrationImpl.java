0840cc1f37a00f8e17d3c059c1133c4e7ebea752
Chesnay Schepler <chesnay@apache.org>
2023-06-01 23:56:26 +0200

    [AUTO-275] Log FlinkIntegration version

--- cc-flink-extensions/cc-flink-integration-impl/src/main/java/io/confluent/flink/jobgraph/FlinkIntegrationImpl.java
+++ cc-flink-extensions/cc-flink-integration-impl/src/main/java/io/confluent/flink/jobgraph/FlinkIntegrationImpl.java
@@ -4,8 +4,20 @@
 
 package io.confluent.flink.jobgraph;
 
+import org.apache.flink.runtime.util.EnvironmentInformation;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /** Default {@link FlinkIntegration} implementation. */
 public class FlinkIntegrationImpl implements FlinkIntegration {
+
+    private static final Logger LOG = LoggerFactory.getLogger(FlinkIntegrationImpl.class);
+
+    public FlinkIntegrationImpl() {
+        LOG.info("Created FlinkIntegration for version {}.", EnvironmentInformation.getVersion());
+    }
+
     @Override
     public JobGraphGenerator getJobGraphGenerator() {
         return new CompiledPlanJobGraphGeneratorImpl();
