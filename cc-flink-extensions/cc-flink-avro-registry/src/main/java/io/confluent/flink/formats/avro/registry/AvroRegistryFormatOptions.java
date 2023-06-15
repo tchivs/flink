2e6a21d7cba1f3adcea979d7b1341de46544cb24
Timo Walther <twalthr@apache.org>
2023-06-15 16:11:23 +0200

    [SQL-1188] Mark avro-registry options as private

--- cc-flink-extensions/cc-flink-avro-registry/src/main/java/io/confluent/flink/formats/avro/registry/AvroRegistryFormatOptions.java
+++ cc-flink-extensions/cc-flink-avro-registry/src/main/java/io/confluent/flink/formats/avro/registry/AvroRegistryFormatOptions.java
@@ -12,18 +12,26 @@ import org.apache.flink.configuration.ConfigOptions;
 @Confluent
 public class AvroRegistryFormatOptions {
 
+    // --------------------------------------------------------------------------------------------
+    // PRIVATE
+    // --------------------------------------------------------------------------------------------
+
+    public static final String PRIVATE_PREFIX = "confluent.";
+
     public static final ConfigOption<String> URL =
-            ConfigOptions.key("url")
+            ConfigOptions.key(PRIVATE_PREFIX + "url")
                     .stringType()
                     .noDefaultValue()
                     .withFallbackKeys("schema-registry.url")
+                    .withFallbackKeys("url")
                     .withDescription(
                             "The URL of the Confluent Schema Registry to fetch/register schemas.");
 
     public static final ConfigOption<Integer> SCHEMA_ID =
-            ConfigOptions.key("schema-id")
+            ConfigOptions.key(PRIVATE_PREFIX + "schema-id")
                     .intType()
                     .noDefaultValue()
+                    .withFallbackKeys("schema-id")
                     .withDescription(
                             "The id of the corresponding Avro schema in Schema Registry."
                                     + " This is important for serialization schema. It ensures the"
@@ -31,27 +39,31 @@ public class AvroRegistryFormatOptions {
                                     + " That way one can control e.g. records namespaces.");
 
     public static final ConfigOption<Integer> SCHEMA_CACHE_SIZE =
-            ConfigOptions.key("schema-cache-size")
+            ConfigOptions.key(PRIVATE_PREFIX + "schema-cache-size")
                     .intType()
                     .defaultValue(20)
+                    .withFallbackKeys("schema-cache-size")
                     .withDescription("Maximum number of cached schemas.");
 
     public static final ConfigOption<String> LOGICAL_CLUSTER_ID =
-            ConfigOptions.key("logical-cluster-id")
+            ConfigOptions.key(PRIVATE_PREFIX + "logical-cluster-id")
                     .stringType()
                     .noDefaultValue()
+                    .withFallbackKeys("logical-cluster-id")
                     .withDescription("The kafka logical cluster id in CC");
 
     public static final ConfigOption<CredentialsSource> CREDENTIALS_SOURCE =
-            ConfigOptions.key("credentials-source")
+            ConfigOptions.key(PRIVATE_PREFIX + "credentials-source")
                     .enumType(CredentialsSource.class)
                     .defaultValue(CredentialsSource.DPAT)
+                    .withFallbackKeys("credentials-source")
                     .withDescription("Where to get the credentials from");
 
     public static final ConfigOption<String> BASIC_AUTH_USER_INFO =
-            ConfigOptions.key("basic-auth.user-info")
+            ConfigOptions.key(PRIVATE_PREFIX + "basic-auth.user-info")
                     .stringType()
                     .noDefaultValue()
+                    .withFallbackKeys("basic-auth.user-info")
                     .withDescription("Basic auth user info for schema registry");
 
     /** Where to get the credentials from. */
