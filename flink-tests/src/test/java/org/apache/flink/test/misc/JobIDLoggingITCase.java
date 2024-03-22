491baec098840f32dd0538cd9b910ba2066e3ae1
Roman Khachatryan <khachatryan.roman@gmail.com>
2024-03-22 16:10:07 +0100

    [FLINK-34643][tests] Ignore 'lost leadership' log messages

--- flink-tests/src/test/java/org/apache/flink/test/misc/JobIDLoggingITCase.java
+++ flink-tests/src/test/java/org/apache/flink/test/misc/JobIDLoggingITCase.java
@@ -152,7 +152,8 @@ class JobIDLoggingITCase {
                 ".*ResourceManager.*",
                 "Operator event.*",
                 "Recovered slot allocation snapshots.*",
-                ".*heartbeat.*");
+                ".*heartbeat.*",
+                ".*leadership.*");
 
         assertJobIDPresent(
                 jobID,
