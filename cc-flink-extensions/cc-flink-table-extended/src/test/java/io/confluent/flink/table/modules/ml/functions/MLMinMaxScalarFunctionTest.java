e9ecfb969ed2845f8105819b893276a8aa9bc858
yanand0909 <yanand@confluent.io>
2024-06-12 12:22:29 -0500

    [MATRIX-213] added java doc comment to MLMinMaxScalarFunctionTest Class

--- cc-flink-extensions/cc-flink-table-extended/src/test/java/io/confluent/flink/table/modules/ml/functions/MLMinMaxScalarFunctionTest.java
+++ cc-flink-extensions/cc-flink-table-extended/src/test/java/io/confluent/flink/table/modules/ml/functions/MLMinMaxScalarFunctionTest.java
@@ -16,6 +16,9 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.Assert.assertThrows;
 
+/**
+ * Unit tests for the {@link MLMinMaxScalarFunction} class.
+ */
 public class MLMinMaxScalarFunctionTest {
 
     private TableEnvironment tableEnv;
